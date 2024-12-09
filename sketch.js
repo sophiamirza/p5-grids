57cb7e44664c657470a142df94ce0c22c0a4a4b4
sophiamirza <sophiamirzauk04@gmail.com>
2024-12-09 12:22:23 +0000

    add random offset to x and y

--- sketch.js
+++ sketch.js
@@ -1,31 +1,68 @@
+// variables
 const canvasSize = 500;
 const squaresPerSide = 10;
 const squareSide = canvasSize / squaresPerSide;
 
+// variables for images
+let texture1;
+let texture2;
+let texture3;
+let texture4;
+
+let textures;
+
+function preload() {
+// load each image
+    texture1 = loadImage("Textures/texture-trans1.png");
+    texture2 = loadImage("Textures/texture-trans2.png");
+    texture3 = loadImage("Textures/texture-trans3.png");
+    texture4 = loadImage("Textures/texture-trans4.png");
+}
+
 function setup() {
   createCanvas(canvasSize, canvasSize);
-  background(220);
+ 
+// stops draw function from repeating continuously
+  noLoop();
+}
+
+function draw() {
+    background(220);
+
+//   add all textures to one array
+  textures = [texture1, texture2, texture3, texture4];
  
 // nested loop 
   for(let column = 0; column < squaresPerSide; column = column + 1){
     for(let row = 0; row < squaresPerSide; row = row + 1){
+
+//  select random texture from array       
+        let randomTexture = random(textures);
     
 // calculate random colour   
         let r = random (0, 255);
         let g = random (0, 255);
         let b = random (0, 255);
-// apply colour   
-        fill (r, g, b);
+// apply colour on squares (not images)  
+        // fill (r, g, b);
+// apply tint (colour) over images
+        tint(r, g, b);
+
+// calculate offset for x (random between -5 and 5)
+        let offsetX = random(-9, 9);
+        let offsetY = random(-9, 9);
+
+// calculate x and y for each column and row position
+        let x = squareSide * column + offsetX;
+        let y = squareSide * row + offsetY;
 
 // calculate x and y for each column and row
-       let x = column * squareSide;
-       let y = row * squareSide;
+    //    let x = column * squareSide;
+    //    let y = row * squareSide;
         
 // draw square
-        square(x, y, 100);
+        // square(x, y, 100);
+        image(randomTexture, x, y, squareSide, squareSide);
     }
   }
-}
-
-function draw() {
 }
